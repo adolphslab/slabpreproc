b2c5d08b562e1a1c709a81340f3a90b8b6846afe
Mike Tyszka <jmtyszka@users.noreply.github.com>
2022-09-07 16:26:57 -0700

    WIP: Start adding PDF report generation code adapted from cbicqc

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2022.9.1-dev',  # Required
+    version='2022.9.7-dev',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
